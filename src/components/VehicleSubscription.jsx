a79cff64fde67b6740fc96ca0320a4ac7f800c70
dedanl <123885283+dedanl@users.noreply.github.com>
2024-08-11 22:50:18 +0300

    new link for backend server

--- src/components/VehicleSubscription.jsx
+++ src/components/VehicleSubscription.jsx
@@ -5,7 +5,7 @@ import { subscribeSocketEvent,unsubscribeSocketEvent } from '../actions/socketAc
 import { receiveVehicleLocation } from '../actions/vehicleActions'
 
 
-const socket =io('https://backend-server-86l5.onrender.com0')
+const socket =io('https://backend-server-86l5.onrender.com')
 
 const VehicleSubscription=()=>{
     const dispatch=useDispatch()
